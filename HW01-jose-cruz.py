07e5c9264231a229877e0453f3e58fbf62a3fab7
Jose J. Cruz Torres <jjzcru@gmail.com>
2021-02-05 18:54:09 -0500

    feat: :sparkles: quote feature

--- HW01-jose-cruz.py
+++ HW01-jose-cruz.py
@@ -31,12 +31,21 @@ def main() -> None:
             break
         player_move = get_player_move()
     display_score_board(score_board)
+    display_random_quote()
 
 
 def start_game(player_move: str, score_board: dict) -> bool:
     """Start a new game depending on the player move"""
 
     # If the move was 'q' exit the game
+    if player_move == 'quote':
+        random_quote = get_random_quote()
+        quote = random_quote['quote']
+        game = random_quote['game']
+        print(f'\nQUOTE 🗣:\n{quote}')
+        print(f' - {game}\n')
+        return True
+
     if player_move == 'q':
         return False
 
@@ -65,6 +74,9 @@ def get_player_move() -> str:
     """Loops until it get a valid player input"""
     move: str = input("Please choose 'R', 'P', 'S' or 'Q' to quit: ")
     move = move.lower()
+
+    if move == 'quote':
+        return move
 
     if move != 'r' and move != 'p' and move != 's' and move != 'q':
         print(f'"{move}" is not a valid option, please try again ⛔️ \n')
@@ -136,6 +148,7 @@ def display_score_board(score_board: dict) -> None:
 
     games_played = total_wins + total_losses + total_ties
     if games_played == 0:
+        clear_screen()
         print(f"You didn't played at all ☹️")
         return
 
@@ -189,6 +202,103 @@ def clear_screen() -> None:
     os.system(shlex_quote('cls' if os.name == 'nt' else 'clear'))
     time.sleep(1)
 
+def display_random_quote():
+    random_quote = get_random_quote()
+    quote = random_quote['quote']
+    game = random_quote['game']
+    print('\n--------------------')
+    print('Quote of the game 📝\n')
+    print(quote)
+    print(f' - {game}\n')
+
+def get_random_quote() -> dict:
+    """Returns a random quote from a game
+    
+    To add a little flavor to the game, when looking at the 
+    score the program is going to display a random quote
+    so players has something to look for everytime they play.
+
+    And who doesn't like a good quote 😌
+    """
+    quotes = [
+        {
+            'quote': 'I’m Commander Shepard, and this is my favorite store on the Citadel!',
+            'game': 'Mass Effect 2'
+        },
+        {
+            'quote': 'What is a man? A Miserable little pile of secrets!',
+            'game': 'Castlevania: Symphony of the Night'
+        },
+        {
+            'quote': 'We’re not tools of the government or anyone else. Fighting was the only thing I was good at, but at least I always fought for what I believed in.',
+            'game': 'Metal Gear Solid'
+        },
+        {
+            'quote': 'The right man in the wrong place can make all the difference in the world.',
+            'game': 'Half-Life 2'
+        },
+        {
+            'quote': 'The healthy human mind doesn’t wake up in the morning thinking this is its last day on Earth. But I think that’s a luxury, not a curse. To know you’re close to the end is a kind of freedom. Good time to take… inventory.',
+            'game': 'Call of Duty: Modern Warfare'
+        },
+        {
+            'quote': 'You can’t break a man the way you break a dog or a horse. The harder you beat a man, the taller he stands.',
+            'game': 'Far Cry 2'
+        },
+        {
+            'quote': 'Nothing is true, everything is permitted.',
+            'game': 'Assassin’s Creed franchise'
+        },
+        {
+            'quote': 'It’s dangerous to go alone, take this!',
+            'game': 'The Legend of Zelda'
+        },
+        {
+            'quote': 'Endure and survive.',
+            'game': 'The Last of Us'
+        },
+        {
+            'quote': 'War is where the young and stupid are tricked by the old and bitter into killing each other.',
+            'game': 'Grand Theft Auto IV'
+        },
+        {
+            'quote': 'A man chooses; a slave obeys.',
+            'game': 'Bioshock'
+        },
+        {
+            'quote': 'Good men mean well. We just don’t always end up doing well.',
+            'game': 'Dead Space 3'
+        },
+        {
+            'quote': 'Men are but flesh and blood. They know their doom, but not the hour.',
+            'game': 'The Elder Scrolls: Oblivion'
+        },
+        {
+            'quote': 'Time passes, people move. Like a river’s flow, it never ends. A childish mind will turn to noble ambition.',
+            'game': 'The Legend of Zelda: Ocarina of Time'
+        },
+        {
+            'quote': 'At the end of the day, as long as there are two people left on the planet, someone is gonna want someone dead.',
+            'game': 'Team Fortress 2'
+        },
+        {
+            'quote': 'Don’t make a girl a promise if you know you can’t keep it.',
+            'game': 'Halo 3'
+        },
+        {
+            'quote': 'The thing about happiness is that you only know you had it when it’s gone. I mean, you may think to yourself that you’re happy. But you don’t believe it. You focus on the petty bull ****, or the next job, or whatever. It’s only looking back by comparison with what comes after that you understand, that’s what happiness felt like.',
+            'game': 'Fallout 4'
+        },
+        {
+            'quote': 'Reliance upon others is a weakness for the strong but strength for the weak. Wisdom and balance lie in knowing your nature over time.',
+            'game': 'Thief II'
+        },
+        {
+            'quote': 'What is a man but the sum of his memories? We are the stories we live, the tales we tell ourselves.',
+            'game': 'Assassin’s Creed: Revelations'
+        }
+    ]
+    return choice(quotes)
 
 if __name__ == "__main__":
     main()
