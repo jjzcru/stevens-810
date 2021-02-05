3d511efab3a726147e327c5ca262764a619c09c8
Jose J. Cruz Torres <jjzcru@gmail.com>
2021-02-05 18:26:12 -0500

    chore: :wastebasket: Code cleanup

--- HW01-jose-cruz.py
+++ HW01-jose-cruz.py
@@ -17,7 +17,7 @@ from shlex import quote as shlex_quote
 from random import choice
 
 
-def main():
+def main() -> None:
     """Main program function"""
     clear_screen()
     player_move = get_player_move()
@@ -28,7 +28,7 @@ def main():
     }
     while True:
         if not start_game(player_move, score_board):
-            break;
+            break
         player_move = get_player_move()
     display_score_board(score_board)
 
@@ -41,7 +41,7 @@ def start_game(player_move: str, score_board: dict) -> bool:
         return False
 
     computer_move: str = get_computer_move()
-    result = get_game_result(player_move, computer_move)
+    result: int = get_game_result(player_move, computer_move)
 
     display_game_result({
         "result": result,
@@ -67,14 +67,18 @@ def get_player_move() -> str:
     move = move.lower()
 
     if move != 'r' and move != 'p' and move != 's' and move != 'q':
-        print(f'"{move}" is not a valid option, please try again ⛔️ ')
+        print(f'"{move}" is not a valid option, please try again ⛔️ \n')
         return get_player_move()
 
     return move
 
 
 def get_computer_move() -> str:
-    """ randomly choose and return one of 'rock', 'paper', 'scissors' """
+    """ randomly choose and return one of 'rock', 'paper', 'scissors'
+        r = rock
+        p = paper
+        s = scissors
+    """
     move: str = choice(['r', 'p', 's'])
     return move
 
@@ -107,9 +111,9 @@ def get_game_result(player_move: str, computer_move: str) -> int:
 
 def display_game_result(game_result: dict) -> None:
     """Prints the game result"""
-    result = game_result['result']
-    player_move = game_result['player_move']
-    computer_move = game_result['computer_move']
+    result: int = game_result['result']
+    player_move: str = game_result['player_move']
+    computer_move: str = game_result['computer_move']
 
     if result == 0:
         print('Tie: We both chose {move}'.format(move=get_move_symbol(player_move)))
@@ -126,18 +130,18 @@ def display_game_result(game_result: dict) -> None:
 
 def display_score_board(score_board: dict) -> None:
     """Receives the score board and prints the result"""
-    total_wins = score_board['wins']
-    total_losses = score_board['losses']
-    total_ties = score_board['ties']
+    total_wins: int = score_board['wins']
+    total_losses: int = score_board['losses']
+    total_ties:int = score_board['ties']
 
     games_played = total_wins + total_losses + total_ties
     if games_played == 0:
         print(f"You didn't played at all ☹️")
         return
 
-    wins_percentage = round((total_wins * 100) / games_played, 2)
-    losses_percentage = round((total_losses * 100) / games_played, 2)
-    ties_percentage = round((total_ties * 100) / games_played, 2)
+    wins_percentage: float = round((total_wins * 100) / games_played, 2)
+    losses_percentage: float = round((total_losses * 100) / games_played, 2)
+    ties_percentage: float = round((total_ties * 100) / games_played, 2)
 
     clear_screen()
     print(f"Games Played: {games_played}\n")
@@ -153,8 +157,9 @@ def display_score_board(score_board: dict) -> None:
 
 def get_message(player_move: str, computer_move: str) -> str:
     """Returns the message depending on game moves"""
-    first_move = ""
-    second_move = ""
+    first_move: str = ""
+    second_move: str = ""
+
     if (player_move == 'r' and computer_move == 's') or (player_move == 's' and computer_move == 'r'):
         first_move = get_move_symbol('r')
         second_move = get_move_symbol('s')
@@ -170,17 +175,16 @@ def get_message(player_move: str, computer_move: str) -> str:
 
 def get_move_symbol(move: str) -> str:
     """It returns the move name and symbol"""
-    if move == 'r':
-        return 'rock 🪨 '
-    if move == 's':
-        return 'scissors ✂ ️'
-    if move == 'p':
-        return 'paper 🧻 '
+    symbols: dict = {
+        'r': 'rock 🪨 ',
+        's': 'scissors ✂️',
+        'p': 'paper 🧻 '
+    }
 
-    return ""
+    return symbols[move]
 
 
-def clear_screen():
+def clear_screen() -> None:
     """Cleans the terminal"""
     os.system(shlex_quote('cls' if os.name == 'nt' else 'clear'))
     time.sleep(1)
