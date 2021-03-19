99015c2095d33d0081fb1ff647208bcab2a2d449
Jose J. Cruz <jjzcru@gmail.com>
2021-03-19 15:36:43 -0400

    feat: :sparkles: Add list_intersect function

--- homeworks/homework 6/HW06_Jose_Cruz.py
+++ homeworks/homework 6/HW06_Jose_Cruz.py
@@ -4,11 +4,16 @@
         Write a function list_copy(l) that takes a list as a parameter and
         returns a copy of the list using a list comprehension.
 
+    Part 2: list_intersect(l1: List[Any], l2: List[Any]) -> List[Any]
+        Write a function that takes two lists as parameters and returns a new
+        list with the values that are included in both lists.
+
    CONVENTIONS:
    - Max character limit per line 80
    - CapWords for class names
    - snake_case for variables and functions
    - function that start with underscore are private
+   - double quotes for strings
 
    Author: Jose J. Cruz
    CWID: 10467076
@@ -19,6 +24,27 @@ from typing import List, Any
 def list_copy(input_list: List[any]) -> List[Any]:
     """Functions receives a list an return a copy"""
     if not isinstance(input_list, List):
-        raise TypeError('variable is not instance of List')
+        raise TypeError("input_list is not instance of List")
 
     return [item for item in input_list]
+
+
+def list_intersect(first_list: List[Any], second_list: List[Any]) -> List[Any]:
+    """Receives two list and return a new list with the intersected values"""
+    if not isinstance(first_list, List):
+        raise TypeError("first_list is not instance of List")
+
+    if not isinstance(second_list, List):
+        raise TypeError("second_list is not instance of List")
+
+    """
+        1. We make copies of the list because we do not want to touch the 
+        originals
+        2. We transform them into a set to get unique values
+        3. We transform the set back to a list
+    """
+    combined_list: List[Any] = list(set(list(first_list) + list(second_list)))
+
+    # In the if segment we ask for items that exist in both lists
+    return [item for item in combined_list if item in first_list
+            and item in second_list]
--- homeworks/homework 6/HW06_Test_Jose_Cruz.py
+++ homeworks/homework 6/HW06_Test_Jose_Cruz.py
@@ -2,11 +2,13 @@
 
     This class will test:
     - list_copy
+    - list_intersect
 
     CONVENTIONS:
     - Max character limit per line 80
     - CapWords for class names
     - snake_case for variables and functions
+    - double quotes for strings
 
    Author: Jose J. Cruz
    CWID: 10467076
@@ -14,11 +16,11 @@
 import unittest
 from typing import List
 
-from HW06_Jose_Cruz import list_copy
+from HW06_Jose_Cruz import list_copy, list_intersect
 
 
 class ListCopyTest(unittest.TestCase):
-    """Test suite for reverse words"""
+    """Test suite for list_copy"""
 
     def test_type_error(self) -> None:
         """Tests that only list are pass as arguments"""
@@ -29,7 +31,7 @@ class ListCopyTest(unittest.TestCase):
     def test_list_copy(self) -> None:
         """Test that all the values in the original list exist in the copy
         and are in the same order"""
-        original_list: List[any] = [1, 'b', True, 9.99]
+        original_list: List[any] = [1, "b", True, 9.99]
         copied_list: List[any] = list_copy(original_list)
         self.assertEqual(original_list, copied_list)
         for index, item in enumerate(original_list):
@@ -38,9 +40,30 @@ class ListCopyTest(unittest.TestCase):
     def test_list_copy_not_same_reference(self) -> None:
         """Test that the copied array do not have the same identity as the
         original"""
-        original_list: List[any] = [1, 'b', True, 9.99]
+        original_list: List[any] = [1, "b", True, 9.99]
         copied_list: List[any] = list_copy(original_list)
         self.assertNotEqual(id(original_list), id(copied_list))
+
+
+class ListIntersectTest(unittest.TestCase):
+    """Test suite for list_intersect"""
+
+    def test_type_error(self) -> None:
+        """Tests that only list are pass as arguments"""
+        self.assertRaises(TypeError, list_intersect, 0)
+        self.assertRaises(TypeError, list_intersect, "a")
+        self.assertRaises(TypeError, list_intersect, 11.1)
+        self.assertRaises(TypeError, list_intersect, [])
+        self.assertRaises(TypeError, list_intersect, [], "a")
+        self.assertRaises(TypeError, list_intersect, [], True)
+        self.assertRaises(TypeError, list_intersect, [], 9)
+
+    def test_list_intersect(self) -> None:
+        """Test that list_intersect"""
+        self.assertEqual(list_intersect([1, 2, 3], [1, 4, 5]), [1])
+        self.assertEqual(list_intersect([], []), [])
+        self.assertEqual(list_intersect(list("abc"), list("axy")), ["a"])
+        self.assertEqual(list_intersect([1, 2, 3, 1], [3, 4, 1, 5]), [1, 3])
 
 
 if __name__ == '__main__':
